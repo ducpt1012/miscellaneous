907a019aa78fade0690176285c9bf3556e7f2ceb
Duc Phan <ducpt1012@gmail.com>
2024-10-14 15:22:26 +0700

    Create clean_zsh_history.sh

---
+++ clean_zsh_history.sh
@@ -0,0 +1,9 @@
+export LC_CTYPE=C
+export LC_CTYPE=C 
+export LANG=C
+
+To remove duplicate lines in .zsh_history:
+
+cat -n .zsh_history | sort -t ';' -uk2 | sort -nk1 | cut -f2- > .zsh_short_history
+
+mv .zsh_short_history .zsh_history
